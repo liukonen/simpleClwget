18c1b0a0d4632b276cffd20d2cb57b6781ac9e27
Luke Liukonen <mfanzone@hotmail.com>
2016-02-15 21:36:30 -0600

    Added a log of enhancements to be made, added multi paramed execution, and multithreading.

--- wget/Module1.vb
+++ wget/Module1.vb
@@ -1,21 +1,47 @@
 ﻿Imports System.Net
+Imports System.Threading.Tasks
+
 Module Module1
 
-    Sub Main()
-        'For Each s In My.Application.CommandLineArgs.ToArray
-        Dim s As String = My.Application.CommandLineArgs(0)
-        'MsgBox(s)
-        Try
-            Dim name As String = s.Substring(s.LastIndexOf("/") + 1).Replace("%", " ")
+    'Enhancements planed.
+    'DONE - 1 if multiple files are passed in, download all of them (for each, which is already there, just commented out
+    'DONE - 2 Multithreading
+    '3 an option, to open a text file and download the contents of the file. (line by line, and erase once complete)
+    '4 an option to indicate a save to directory
+    '5 an override parameter to indicate how many threads to use (default 3)
+    '6 external error logging
 
-            Console.WriteLine(("Downloading " & s))
-            Dim client As New WebClient
-            client.DownloadFile(s, name)
+    Sub Main()
+
+        Dim Errors As New Concurrent.ConcurrentBag(Of Exception)()
+
+        Try
+            Dim PO As New Threading.Tasks.ParallelOptions()
+            PO.MaxDegreeOfParallelism = 3
+            Parallel.ForEach(My.Application.CommandLineArgs.ToArray, PO, Sub(ByVal DownloadLink As String)
+                                                                             Dim FileName As String = DownloadLink.Substring(DownloadLink.LastIndexOf("/") + 1).Replace("%", " ")
+                                                                             Try
+                                                                                 If Not IO.File.Exists(FileName) Then
+                                                                                     Console.WriteLine("Downloading file: " & FileName)
+                                                                                     Using client As New WebClient()
+                                                                                         client.DownloadFile(DownloadLink, FileName)
+                                                                                     End Using
+                                                                                 Else
+                                                                                     Console.WriteLine("Can not download file because it already exists:" & FileName)
+                                                                                 End If
+                                                                             Catch ex As Exception
+                                                                                 Errors.Add(ex)
+                                                                             End Try
+                                                                         End Sub)
         Catch ex As Exception
-            Console.WriteLine(ex)
+            Errors.Add(ex)
         End Try
 
-        'Next
+        If Errors.Any Then
+            For Each X As Exception In Errors
+                Console.WriteLine(X.ToString())
+            Next
+        End If
     End Sub
 
 End Module
