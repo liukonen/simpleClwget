1d452402ee07048fce49111132f195afd1eddfca
Luke Liukonen <mfanzone@hotmail.com>
2016-02-15 21:10:50 -0600

    first Checkin

---
+++ wget/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Net
+Module Module1
+
+    Sub Main()
+        'For Each s In My.Application.CommandLineArgs.ToArray
+        Dim s As String = My.Application.CommandLineArgs(0)
+        'MsgBox(s)
+        Try
+            Dim name As String = s.Substring(s.LastIndexOf("/") + 1).Replace("%", " ")
+
+            Console.WriteLine(("Downloading " & s))
+            Dim client As New WebClient
+            client.DownloadFile(s, name)
+        Catch ex As Exception
+            Console.WriteLine(ex)
+        End Try
+
+        'Next
+    End Sub
+
+End Module
---
+++ wget/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("wget")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Hewlett-Packard Company")> 
+<Assembly: AssemblyProduct("wget")> 
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c06e701b-7434-4df0-b7b0-64d1bdaf632f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
